48882e24450ebec46cb0cd3b6716bd7d1ff73919
ofcoursedude <ofcoursedude@users.noreply.github.com>
2022-06-15 16:49:44 +0200

    Support pipe input (#3)

--- src/tobase64/Program.cs
+++ src/tobase64/Program.cs
@@ -1,10 +1,35 @@
-﻿using System.Text;
+﻿using System.Buffers;
+using System.Text;
 
 if (args.Length == 0)
 {
-    Console.WriteLine("Usage: tobase64 <string-to-convert>");
-    return;
+    var pipeInput = Console.OpenStandardInput();
+
+    var buffer = ArrayPool<byte>.Shared.Rent(3072);
+    var pipeInputTextTask = pipeInput.ReadAsync(buffer, 0, 3);
+    await Task.WhenAny(pipeInputTextTask, Task.Delay(TimeSpan.FromMilliseconds(100)));
+    if (pipeInputTextTask.IsCompleted)
+    {
+        var text = Encoding.UTF8.GetString(buffer, 0, await pipeInputTextTask);
+        Console.Write(Convert.ToBase64String(Encoding.UTF8.GetBytes(text)));
+        var count = await pipeInput.ReadAsync(buffer.AsMemory(0, 3072));
+        while (count > 0)
+        {
+            Console.Write(Convert.ToBase64String(buffer, 0, count));
+            count = await pipeInput.ReadAsync(buffer.AsMemory(0, 3072));
+        }
+    }
+    else
+    {
+        Console.WriteLine("Usage: tobase64 <string-to-convert>");
+        Console.WriteLine("       or pipe string to stdin");
+        return;
+    }
+    ArrayPool<byte>.Shared.Return(buffer);
 }
-var sourceString = string.Join(" ", args);
-var base64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(sourceString));
-Console.WriteLine(base64);
+else
+{
+    var sourceString = string.Join(" ", args);
+    var base64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(sourceString));
+    Console.WriteLine(base64);
+}
