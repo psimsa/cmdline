60d90612dde887681d53f4eee18a91c319fad772
Pavel Simsa <pavel.simsa@vistaprint.com>
2022-06-15 14:27:10 +0200

    Initial commit

---
+++ src/tobase64/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+
+if (args.Length == 0)
+{
+    Console.WriteLine("Usage: tobase64 <string-to-convert>");
+    return;
+}
+var sourceString = string.Join(" ", args);
+var base64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(sourceString));
+Console.WriteLine(base64);
