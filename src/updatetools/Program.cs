be74c550af490add0e73929ad936d2807f5406d3
ofcoursedude <ofcoursedude@users.noreply.github.com>
2022-06-15 18:36:02 +0200

    Add update tool (#7)

---
+++ src/updatetools/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+var version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "'unknown'";
+Console.WriteLine($"{Assembly.GetExecutingAssembly().GetName().Name} version {version}");
+
+string globalSwitch = "";
+switch (args.Length)
+{
+    case 0:
+        globalSwitch = "";
+        break;
+    case 1 when args[0] == "--global" || args[0] == "-g":
+        globalSwitch = "-g";
+        break;
+    default:
+        Console.WriteLine("Usage: dotnet updatetools [-g|--global]");
+        Environment.Exit(1);
+        break;
+}
+
+var outputLines = new List<string>();
+
+var getToolsListProcess = new Process()
+{
+    StartInfo =
+    {
+        FileName = "dotnet",
+        Arguments = $"tool list {globalSwitch}",
+        UseShellExecute = false,
+        RedirectStandardOutput = true,
+        RedirectStandardError = false,
+        CreateNoWindow = true
+    }
+};
+
+getToolsListProcess.OutputDataReceived += (sender, e) =>
+{
+    if (e.Data != null)
+    {
+        outputLines.Add(e.Data);
+    }
+};
+
+getToolsListProcess.Start();
+getToolsListProcess.BeginOutputReadLine();
+getToolsListProcess.WaitForExit();
+
+Parallel.ForEach(
+    outputLines.Skip(2).Select(l => l.Split(' ', 2)[0]),
+    new ParallelOptions() {MaxDegreeOfParallelism = 4},
+    tool =>
+    {
+        var updateProcess = new Process()
+        {
+            StartInfo =
+            {
+                FileName = "dotnet",
+                Arguments = $"tool update {tool} {globalSwitch}",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = false,
+                CreateNoWindow = true
+            }
+        };
+
+        updateProcess.OutputDataReceived += (sender, e) =>
+        {
+            if (e.Data != null)
+            {
+                Console.WriteLine(e.Data);
+            }
+        };
+        updateProcess.Start();
+        updateProcess.BeginOutputReadLine();
+        var exited = updateProcess.WaitForExit(60_000);
+        if (!exited) updateProcess.Kill(true);
+    });
---
+++ src/updatetools/README.md
@@ -0,0 +1,5 @@
+# updatetools
+dotnet-updatetools is a tool that updates all local or global tools to the latest version. Runs in 4 processes and times out an individual update after 1 minute.
+
+
+Part of a set of command line tools at https://github.com/ofcoursedude/cmdline
